4e7099a5204a754aa4a8767b36aa6114f8d1b01a
ldmachad <leonardodiomachado@gmail.com>
2023-10-12 16:49:34 -0300

    CRUD finalizado

--- DrogaBoa/Program.cs
+++ DrogaBoa/Program.cs
@@ -9,7 +9,6 @@ using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers()
                 .AddNewtonsoftJson(options =>
                 {
